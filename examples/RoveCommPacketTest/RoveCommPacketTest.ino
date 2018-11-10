f1291eb5404e6bce7100e0fb41813bc95ed456d3
andrewvh4 <31864022+andrewvh4@users.noreply.github.com>
2018-11-09 21:10:24 -0600

    Created Test Code

--- examples/RoveCommPacketTest/RoveCommPacketTest.ino
+++ examples/RoveCommPacketTest/RoveCommPacketTest.ino
@@ -1,69 +1,230 @@
 //RoveComm Example
-//Sends single value and array entry to roveComm
+//Used for testing RoveComm transfer commands between the TIVA and a laptop running a Python Script
 //Reads array entry from RoveComm
 //Andrew Van Horn, 11/2018
-/*The following values used in this example are defined in RoveManifest.h
- * const uint8_t EXAMPLE_FOURTH_OCTET          = 141;
- * const uint16_t SINGLE_VALUE_EXAMPLE_ID = 10;
- * const uint16_t ARRAY_ENTRY_EXAMPLE_ID  = 11;
- * const uint16_t ARRAY_READ_EXAMPLE_ID   = 12;
- */
 
 #include "RoveCommEthernetUdp.h"
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 RoveCommEthernetUdp RoveComm;
 struct rovecomm_packet rovecomm_read_packet;
+int count = 0;
 
-#define OUTPUT_SINGLE_ENTRY  1
-#define OUTPUT_ARRAY_ENTRY   1
+uint16_t data_id = 10;
 
 void setup() 
 {
   RoveComm.begin(EXAMPLE_FOURTH_OCTET);
   Serial.begin(9600);
   delay(10);
-  pinMode(A0, INPUT);
-  pinMode(A1, INPUT);
-  pinMode(A2, INPUT);
   Serial.println("Initialised");
   delay(10);
+  Serial.println("Count,Data ID,Data Count,Data");
 }
 
 void loop()
 {
-  if(OUTPUT_SINGLE_ENTRY)
-  {
-    RoveComm.write(SINGLE_VALUE_EXAMPLE_ID, 1, analogRead(A0));
-    Serial.print("Writing: ");
-    Serial.println(analogRead(A0));
-    delay(10);
-  }
 
-  if(OUTPUT_ARRAY_ENTRY)
-  {
-    uint16_t send_data[] = {analogRead(A0),analogRead(A1), analogRead(A2)};
-    Serial.println("Writing Array");
-    RoveComm.write(SINGLE_VALUE_EXAMPLE_ID, 3, send_data);
-    delay(10);
-  }
+ /*TIVA send
+ int count = 0;
+ rovecomm_read_packet = RoveComm.read();
+ if(rovecomm_read_packet.data_id == 1)
+ {
+   //Test Data IDs
+   int8_t data = 1;
+   count++;
+   RoveComm.write(data_id, 1, data);
+   Serial.print(count);
+   Serial.print(",");
+   Serial.print(data_id);
+   Serial.print(",");
+   Serial.print(1);
+   Serial.print(",");
+   Serial.println(data);
+   data_id = 1000;
+   count++;
+   RoveComm.write(data_id, 1, data);
+   Serial.print(count);
+   Serial.print(",");
+   Serial.print(data_id);
+   Serial.print(",");
+   Serial.print(1);
+   Serial.print(",");
+   Serial.println(data);
+   data_id = 2000;
+   count++;
+   RoveComm.write(data_id, 1, data);
+   Serial.print(count);
+   Serial.print(",");
+   Serial.print(data_id);
+   Serial.print(",");
+   Serial.print(1);
+   Serial.print(",");
+   Serial.println(data);
+   data_id = 3000;
+   count++;
+   RoveComm.write(data_id, 1, data);
+   Serial.print(count);
+   Serial.print(",");
+   Serial.print(data_id);
+   Serial.print(",");
+   Serial.print(1);
+   Serial.print(",");
+   Serial.println(data);
+   data_id = 65000;
+   count++;
+   RoveComm.write(data_id, 1, data);
+   Serial.print(count);
+   Serial.print(",");
+   Serial.print(data_id);
+   Serial.print(",");
+   Serial.print(1);
+   Serial.print(",");
+   Serial.println(data);
+   //Test Random IDs
+   for(int i = 0; i<4; i++)
+   {
+     data_id = random(0, 65000);
+     count++;
+     RoveComm.write(data_id, 1, data);
+     Serial.print(count);
+     Serial.print(",");
+     Serial.print(data_id);
+     Serial.print(",");
+     Serial.print(1);
+     Serial.print(",");
+     Serial.println(data);
+   }
+   //Test Data Types
+   data_id = 10;
+   for(int i = 0; i<10; i++)
+   {
+     uint8_t data_array[] = {random(0, 255), random(0, 255), random(0, 255)};
+     count++;
+     RoveComm.write(data_id, 3, data_array);
+     Serial.print(count);
+     Serial.print(",");
+     Serial.print(data_id);
+     Serial.print(",");
+     Serial.print(3);
+     Serial.print(",");
+     for(int j = 0; j<3; j++)
+     {
+     Serial.print(data_array[j]);
+     Serial.print(",");
+     }
+     Serial.println("");
+   }
+   for(int i = 0; i<10; i++)
+   {
+     int8_t data_array[] = {random(-127, 127), random(-127, 127), random(-127, 127)};
+     count++;
+     RoveComm.write(data_id, 3, data_array);
+     Serial.print(count);
+     Serial.print(",");
+     Serial.print(data_id);
+     Serial.print(",");
+     Serial.print(3);
+     Serial.print(",");
+     for(int j = 0; j<3; j++)
+     {
+     Serial.print(data_array[j]);
+     Serial.print(",");
+     }
+     Serial.println("");
+   }
+   for(int i = 0; i<10; i++)
+   {
+     uint16_t data_array[] = {random(0, 60000), random(0, 60000), random(0, 60000)};
+     count++;
+     RoveComm.write(data_id, 3, data_array);
+     Serial.print(count);
+     Serial.print(",");
+     Serial.print(data_id);
+     Serial.print(",");
+     Serial.print(3);
+     Serial.print(",");
+     for(int j = 0; j<3; j++)
+     {
+     Serial.print(data_array[j]);
+     Serial.print(",");
+     }
+     Serial.println("");
+   }
+   for(int i = 0; i<10; i++)
+   {
+     int16_t data_array[] = {random(-32768, 32767), random(-32768, 32767), random(-32768, 32767)};
+     count++;
+     RoveComm.write(data_id, 3, data_array);
+     Serial.print(count);
+     Serial.print(",");
+     Serial.print(data_id);
+     Serial.print(",");
+     Serial.print(3);
+     Serial.print(",");
+     for(int j = 0; j<3; j++)
+     {
+     Serial.print(data_array[j]);
+     Serial.print(",");
+     }
+     Serial.println("");
+   }
+   for(int i = 0; i<10; i++)
+   {
+     uint32_t data_array[] = {random(0, 2000000000), random(0, 2000000000), random(0, 2000000000)};
+     count++;
+     RoveComm.write(data_id, 3, data_array);
+     Serial.print(count);
+     Serial.print(",");
+     Serial.print(data_id);
+     Serial.print(",");
+     Serial.print(3);
+     Serial.print(",");
+     for(int j = 0; j<3; j++)
+     {
+     Serial.print(data_array[j]);
+     Serial.print(",");
+     }
+     Serial.println("");
+   }
+   for(int i = 0; i<10; i++)
+   {
+     int32_t data_array[] = {random(-1000000000, 1000000000), random(-1000000000, 1000000000), random(-1000000000, 1000000000)};
+     count++;
+     RoveComm.write(data_id, 3, data_array);
+     Serial.print(count);
+     Serial.print(",");
+     Serial.print(data_id);
+     Serial.print(",");
+     Serial.print(3);
+     Serial.print(",");
+     for(int j = 0; j<3; j++)
+     {
+     Serial.print(data_array[j]);
+     Serial.print(",");
+     }
+     Serial.println("");
+   }
+ }
+ */
+ /* TIVA Read Code
+ delay(1);
+ rovecomm_read_packet = RoveComm.read();
+ if(rovecomm_read_packet.data_id != 0)
+ {
+ count ++;
+   Serial.print(count);
+   Serial.print(",");
+   Serial.print(rovecomm_read_packet.data_id);
+   Serial.print(",");
+   Serial.print(rovecomm_read_packet.data_count);
+   Serial.print(",");
+   for(uint8_t i = 0; i< rovecomm_read_packet.data_count; i++)
+    {
+      Serial.print(rovecomm_read_packet.data[i]);
+      Serial.print(",");
+    }
   
-  rovecomm_read_packet =RoveComm.read();
-
-  if(rovecomm_read_packet.data_id != 0)
-  {
-    Serial.print("Packet Read: ");
-    Serial.println(rovecomm_read_packet.data_id);
-  }
-  
-  switch(rovecomm_read_packet.data_id)
-  {
-    case (ARRAY_READ_EXAMPLE_ID):
-      for(uint8_t i = 0; i< rovecomm_read_packet.data_count; i++)
-      {
-        Serial.println(rovecomm_read_packet.data[i]);
-      }
-    default:
-     delay(1000);
-  }
-  
+   Serial.println("");
+ }
+ */
 }
