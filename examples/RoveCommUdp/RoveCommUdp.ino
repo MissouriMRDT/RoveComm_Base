598781b0cd9a763417cc58a543d2ef71a6ab6246
Eli Verbrugge <eli.verbrugge@gmail.com>
2020-02-18 23:30:19 -0600

    Updated example to display functionality of casting packet data to data type

--- examples/RoveCommUdp/RoveCommUdp.ino
+++ examples/RoveCommUdp/RoveCommUdp.ino
@@ -1,23 +1,22 @@
-//Example usage of RoveComm.write for sending Telemetry
-//Example of array and single-valued telemetry using BMSBoard as an example
-//Andrew Van Horn
-//2019-01-29
+//Example of receiving and writing rovecomm data for a mock driveboard
 
 #include "RoveComm.h"
 
 RoveCommEthernet RoveComm;
 rovecomm_packet packet;
 
-int16_t speed[6] = {-500, 200, 740, -720, 10, -182};
-void setup() {
+int16_t motor_speed[6] = {-500, 200, 740, -720, 10, -182};
+
+void setup() 
+{
   Serial.begin(9600);
   RoveComm.begin(RC_DRIVEBOARD_FOURTHOCTET, RC_ROVECOMM_ETHERNET_DRIVE_LIGHTING_BOARD_PORT);
   delay(100);
-  Serial.println("Started");
-
+  Serial.println("Started: ");
 }
 
-void loop() {
+void loop() 
+{
   delay(100);
   packet = RoveComm.read();
   Serial.println(packet.data_id);
@@ -27,14 +26,30 @@ void loop() {
       Serial.println("We received an individual wheel drive command");
       break;
     case RC_DRIVEBOARD_DRIVELEFTRIGHT_DATAID:
-      Serial.println("We received a left/right drive command");
+      //cast the packet to the correct data type
+      int16_t* speeds = (int16_t*)packet.data;
+      Serial.println("We received a left/right drive command:");
+      //Serial.println(printf("Left: %d", speeds[0]));
+      //Serial.println(printf("Right: %d", speeds[1]));
+
+      //set the motor speeds to the commanded speeds in RoveComm
+      motor_speed[0] = speeds[0];
+      motor_speed[1] = speeds[0];
+      motor_speed[2] = speeds[0];
+      motor_speed[3] = speeds[1];
+      motor_speed[4] = speeds[1];
+      motor_speed[5] = speeds[1];
       break;
     default:
       Serial.println("Unexpected data id: ");
       Serial.println(packet.data_id);
       break;
   }
+
+  //Code to drive motors goes here
+
+
   //Send mock telemetry for drive speeds
-  RoveComm.write(RC_DRIVEBOARD_DRIVE_SPEED_DATAID, RC_DRIVEBOARD_DRIVE_SPEED_DATACOUNT, speed);
+  RoveComm.write(RC_DRIVEBOARD_DRIVE_SPEED_DATAID, RC_DRIVEBOARD_DRIVE_SPEED_DATACOUNT, motor_speed);
 }
 
