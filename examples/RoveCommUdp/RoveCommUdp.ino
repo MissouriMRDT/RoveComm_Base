81ddb7d4c713b6f227b5fb1b3b55a7aacd49259e
Eli Verbrugge <eli.verbrugge@gmail.com>
2020-08-29 23:30:45 -0500

    Updated example code for UDP, to be clearer in usage and include EthernetServer declaration

--- examples/RoveCommUdp/RoveCommUdp.ino
+++ examples/RoveCommUdp/RoveCommUdp.ino
@@ -10,12 +10,15 @@ int16_t motor_speed[6] = {-500, 200, 740, -720, 10, -182};
 //timekeeping variables
 uint32_t last_update_time;
 
+//declare the Ethernet Server in the top level sketch with the requisite data ID any time you want to use RoveComm
+EthernetServer TCPServer(RC_ROVECOMM_ETHERNET_DRIVE_LIGHTING_BOARD_PORT);
+
 void setup() 
 {
   Serial.begin(9600);
 
   //Set up rovecomm with the correct IP and the TCP server
-  RoveComm.begin(RC_DRIVEBOARD_FOURTHOCTET, RC_ROVECOMM_ETHERNET_DRIVE_LIGHTING_BOARD_PORT);
+  RoveComm.begin(RC_DRIVEBOARD_FOURTHOCTET, &TCPServer);
   delay(100);
 
   Serial.println("Started: ");
@@ -38,8 +41,15 @@ void loop()
       break;
     case RC_DRIVEBOARD_DRIVELEFTRIGHT_DATAID:
       //cast the packet to the correct data type
-      int16_t* speeds = (int16_t*)packet.data;
+      int16_t* speeds;
+      speeds = (int16_t*)packet.data;
+      
+      //print out speeds nicely formatted
       Serial.println("We received a left/right drive command:");
+      
+      char buf[100];
+      sprintf(buf, "Left: %d, Right: %d", speeds[0], speeds[1]);
+      Serial.println(buf);
 
       //set the motor speeds to the commanded speeds in RoveComm
       motor_speed[0] = speeds[0];
