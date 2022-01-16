10ce1c70e60d1354efeb0afd458b2a35748ea25d
Anthony Robles <antrobles254@gmail.com>
2022-01-16 16:20:36 -0600

    Feature/teensy rove comm (#6)
    * First intial Teensy RoveComm Integration, still need to organize the folder without causing eternal blindness
    
    * Pain.
    
    * Removed Energia.h and replaced ethernet with nativeEthernet
    
    * Fixed TeensyRoveCommUDP
    
    examples/TeensyRoveCommUDP now properly sends/receives UDP data
    
    * Removed extra files for Teensy
    
    * Cleaned up TeensyRoveCommUDP example
    
    * Cleaned up Teensy src directory and added the new manifest to prepare for a merge with dev
    
    Co-authored-by: Liam-Helfrich <whn7g@mst.edu>

---
+++ src/srcTeensy/TeensyRoveComm.h
@@ -0,0 +1,9 @@
+#ifndef TeensyRoveComm
+#define TeensyRoveComm
+
+#include "TeensyRoveCommEthernet.h"
+#include "TeensyRoveCommEthernetUdp.h"
+#include "TeensyRoveCommEthernetTCP.h"
+#include "TeensyRoveCommSerial.h"
+
+#endif
